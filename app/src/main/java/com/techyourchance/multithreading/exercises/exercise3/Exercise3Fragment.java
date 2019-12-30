68d61a789789b09a12e5011e52d20381981441b5
Alvin Dizon <alvin.dizon91@gmail.com>
2019-12-30 15:56:23 +0800

    Solution to exercise 3
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- app/src/main/java/com/techyourchance/multithreading/exercises/exercise3/Exercise3Fragment.java
+++ app/src/main/java/com/techyourchance/multithreading/exercises/exercise3/Exercise3Fragment.java
@@ -7,12 +7,14 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.techyourchance.multithreading.R;
-import com.techyourchance.multithreading.common.BaseFragment;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.techyourchance.multithreading.R;
+import com.techyourchance.multithreading.common.BaseFragment;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Exercise3Fragment extends BaseFragment {
 
@@ -24,6 +26,7 @@ public class Exercise3Fragment extends BaseFragment {
 
     private Button mBtnCountSeconds;
     private TextView mTxtCount;
+    private AtomicBoolean countBoolean = new AtomicBoolean(false);
 
     @Nullable
     @Override
@@ -44,6 +47,21 @@ public class Exercise3Fragment extends BaseFragment {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        // ensure that button is enabled when exercise 3 screen is first displayed
+        mBtnCountSeconds.setEnabled(true);
+        // set flag to true in order for count runnable to execute
+        countBoolean.set(true);
+    }
+
+    @Override
+    public void onStop() {
+        countBoolean.set(false);
+        super.onStop();
+    }
+
+    @Override
     protected String getScreenTitle() {
         return "Exercise 3";
     }
@@ -55,5 +73,28 @@ public class Exercise3Fragment extends BaseFragment {
         3. Show count in TextView
         4. When count completes, show "done" in TextView and enable the button
          */
+        new Thread(() -> {
+            runOnUiThread(() -> mBtnCountSeconds.setEnabled(false));
+            for(int i = 1; i <= SECONDS_TO_COUNT; i++) {
+                if(!countBoolean.get()) {
+                    return; // return if fragment is not displayed on screen
+                }
+                final int count = i;
+                runOnUiThread(() -> mTxtCount.setText("Count: " + count));
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            runOnUiThread(() -> mBtnCountSeconds.setEnabled(true));
+            runOnUiThread(() -> mTxtCount.setText("Done counting"));
+        }).start();
+    }
+
+    private void runOnUiThread(Runnable runnable) {
+        if(countBoolean.get()) { // if screen has returned to main menu, do not post runnable to UI thread
+            requireActivity().runOnUiThread(runnable);
+        }
     }
 }
