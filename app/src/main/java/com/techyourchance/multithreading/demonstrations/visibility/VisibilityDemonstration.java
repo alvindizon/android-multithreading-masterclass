d3cd69b15b78164a169d919035debbde0456f124
Alvin Dizon <alvin.dizon91@gmail.com>
2020-01-01 11:52:45 +0800

    Introduce manual synchronization to address Consumer thread reading incorrect values
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- app/src/main/java/com/techyourchance/multithreading/demonstrations/visibility/VisibilityDemonstration.java
+++ app/src/main/java/com/techyourchance/multithreading/demonstrations/visibility/VisibilityDemonstration.java
@@ -2,6 +2,8 @@ package com.techyourchance.multithreading.demonstrations.visibility;
 
 public class VisibilityDemonstration {
 
+    private static final Object LOCK = new Object();
+
     private static int sCount = 0;
 
     public static void main(String[] args) {
@@ -19,12 +21,14 @@ public class VisibilityDemonstration {
         public void run() {
             int localValue = -1;
             while (true) {
-                if (localValue != sCount) {
-                    System.out.println("Consumer: detected count change " + sCount);
-                    localValue = sCount;
-                }
-                if (sCount >= 5) {
-                    break;
+                synchronized (LOCK) {
+                    if (localValue != sCount) {
+                        System.out.println("Consumer: detected count change " + sCount);
+                        localValue = sCount;
+                    }
+                    if (sCount >= 5) {
+                        break;
+                    }
                 }
             }
             System.out.println("Consumer: terminating");
@@ -34,15 +38,15 @@ public class VisibilityDemonstration {
     static class Producer extends Thread {
         @Override
         public void run() {
-            while (sCount < 5) {
-                int localValue = sCount;
-                localValue++;
-                System.out.println("Producer: incrementing count to " + localValue);
-                sCount = localValue;
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    return;
+            while (true) {
+                synchronized (LOCK) { // hold this LOCK until the code inside synchronized block completes
+                    if(sCount >= 5) { // transferred read of sCount inside synchronized block to make code thread-safe
+                        break;
+                    }
+                    int localValue = sCount;
+                    localValue++;
+                    System.out.println("Producer: incrementing count to " + localValue);
+                    sCount = localValue;
                 }
             }
             System.out.println("Producer: terminating");
